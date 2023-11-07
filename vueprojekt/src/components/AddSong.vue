cce3d39e40140caaa805990fcbfdc2aa441c1782
NadiaReedtz <nreedtzj@gmail.com>
2023-11-07 17:35:30 +0100

    musiclist

---
+++ vueprojekt/src/components/AddSong.vue
@@ -0,0 +1,30 @@
+<template>
+  <form @submit.prevent="addSong">
+    <input type="text" v-model="newSong.title" placeholder="Titel" required>
+    <input type="text" v-model="newSong.artist" placeholder="Artist" required>
+    <!-- Tilføj flere input-felter efter behov -->
+    <button type="submit">Tilføj Sang</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newSong: {
+        title: '',
+        artist: '',
+        // andre felter
+      },
+      // Resten af din data...
+    };
+  },
+  methods: {
+    addSong() {
+      this.songs.push(this.newSong);
+      this.newSong = { title: '', artist: '' }; // Reset input felter
+      // Her kunne du også tilføje logik til at persistere den nye sang i en database eller en lokal storage.
+    },
+  },
+};
+</script>
--- vueprojekt/src/components/MusicList.vue
+++ vueprojekt/src/components/MusicList.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <h1>Musikliste</h1>
+    <input v-model="search.artist" placeholder="Søg efter artist">
+    <input v-model="search.album" placeholder="Søg efter album">
+    <input v-model="search.year" placeholder="Søg efter år">
+    <input v-model="search.genre" placeholder="Søg efter genre">
+
     <ul>
-      <li v-for="item in musicData" :key="item.id">
-        {{ item.artist }} - {{ item.album }} ({{ item.year }})
+      <li v-for="song in filteredMusic" :key="song.id">
+        {{ song.artist }} - {{ song.album }} ({{ song.year }}) - {{ song.genre }}
       </li>
     </ul>
   </div>
@@ -16,8 +21,26 @@ export default {
   name: 'MusicList',
   data() {
     return {
-      musicData
+      musicData,
+      search: {
+        artist: '',
+        album: '',
+        year: '',
+        genre: ''
+      }
     };
+  },
+  computed: {
+    filteredMusic() {
+      return this.musicData.filter((song) => {
+        return (
+          (this.search.artist ? song.artist.toLowerCase().includes(this.search.artist.toLowerCase()) : true) &&
+          (this.search.album ? song.album.toLowerCase().includes(this.search.album.toLowerCase()) : true) &&
+          (this.search.year ? song.year.toString().includes(this.search.year) : true) &&
+          (this.search.genre ? song.genre.toLowerCase().includes(this.search.genre.toLowerCase()) : true)
+        );
+      });
+    }
   }
 };
 </script>
